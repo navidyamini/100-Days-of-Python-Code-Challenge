1aa0c60befeba35c32bfb878989489265e5d0270
Navid Yamini <navidyamini@gmail.com>
2024-01-09 23:46:58 +0100

    Delete Day 1/band_name_generator.py

--- Day 1/band_name_generator.py
+++
@@ -1,4 +0,0 @@
-print("Welcome to the Band Name Generator.")
-city_name = input("What's name of the city you grew up in?\n")
-pet_name = input("What's your pet's name?\n")
-print("Your band name could be " + city_name + ' ' + pet_name)
