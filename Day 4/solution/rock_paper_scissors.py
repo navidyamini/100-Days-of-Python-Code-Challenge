bc5b39a95a4aabf11d84a7e177a1cb1f22b319c4
Navid Yamini <navidyamini@gmail.com>
2024-01-11 23:46:12 +0100

    Update rock_paper_scissors.py

--- Day 4/solution/rock_paper_scissors.py
+++ Day 4/solution/rock_paper_scissors.py
@@ -26,14 +26,14 @@ scissors = '''
       (____)
 ---.__(___)
 '''
+choices = [rock, paper, scissors]
 print("Rock Paper Scissors")
 user_choice = int(input("What do you choose? Type 0 for Rock, 1 for Paper or 2 for Scissors:\n"))
-choices = [rock, paper, scissors]
 computer_choice = random.randint(0, 2)
 if (user_choice >= 3 or user_choice < 0):
     print("WRONG INPUT!")
 else:
-    print("You choosed " + choices[user_choice] + " and computer choosed " + choices[computer_choice])
+    print("You chose " + choices[user_choice] + " and computer chose " + choices[computer_choice])
     if user_choice == computer_choice:
         print("Draw!")
     elif (user_choice == 0 and computer_choice == 1) or (user_choice == 1 and computer_choice == 2) or (user_choice == 2 and computer_choice == 0):
