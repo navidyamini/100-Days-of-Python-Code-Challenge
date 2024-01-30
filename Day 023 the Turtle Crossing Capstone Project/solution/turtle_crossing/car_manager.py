f030e993a58d3d0ea22ee113edf43cb2de26e10f
Navid Yamini <navidyamini@gmail.com>
2024-01-30 23:09:51 +0100

    Update car_manager.py

--- Day 023 the Turtle Crossing Capstone Project/solution/turtle_crossing/car_manager.py
+++ Day 023 the Turtle Crossing Capstone Project/solution/turtle_crossing/car_manager.py
@@ -24,7 +24,7 @@ class CarManager:
 
     def move_cars(self):
         for car in self.all_cars:
-            car.backward(STARTING_MOVE_DISTANCE)
+            car.backward((self.car_speed))
 
     def level_up(self):
         self.car_speed += MOVE_INCREMENT
