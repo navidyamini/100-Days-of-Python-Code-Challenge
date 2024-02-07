31cdd363a9f141080d120ecfe326caa49ba1be62
Navid Yamini <navidyamini@gmail.com>
2024-02-07 23:38:29 +0100

    Update main.py

---
+++ Day 30 Errors, Exceptions and JSON Data, Improving Password Project/solution/main.py
@@ -0,0 +1,16 @@
+# FileNotFound
+try:
+    file = open("a_file.txt")
+    a_dictionary = {"key": "value"}
+    print(a_dictionary["some_key"])
+except FileNotFoundError:
+    file = open("a_file.txt", "w")
+    file.write("Something")
+except KeyError as error_message:
+    print(f"The key {error_message} does not exist.")
+else:
+    content = file.read()
+    print(content)
+finally:
+    file.close()
+    print('File was closed')
