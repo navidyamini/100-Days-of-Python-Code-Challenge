8e5e67eccfe1efc6801950e97e80d89b91e41be4
Navid Yamini <navidyamini@gmail.com>
2024-02-08 19:58:38 +0100

    Update main.py

--- Day 30 Errors, Exceptions and JSON Data, Improving Password Project/solution/main.py
+++ Day 30 Errors, Exceptions and JSON Data, Improving Password Project/solution/main.py
@@ -1,16 +1,29 @@
-# FileNotFound
-try:
-    file = open("a_file.txt")
-    a_dictionary = {"key": "value"}
-    print(a_dictionary["some_key"])
-except FileNotFoundError:
-    file = open("a_file.txt", "w")
-    file.write("Something")
-except KeyError as error_message:
-    print(f"The key {error_message} does not exist.")
-else:
-    content = file.read()
-    print(content)
-finally:
-    file.close()
-    print('File was closed')
+# # FileNotFound
+# try:
+#     file = open("a_file.txt")
+#     a_dictionary = {"key": "value"}
+#     print(a_dictionary["some_key"])
+# except FileNotFoundError:
+#     file = open("a_file.txt", "w")
+#     file.write("Something")
+# except KeyError as error_message:
+#     print(f"The key {error_message} does not exist.")
+# else:
+#     content = file.read()
+#     print(content)
+# finally:
+#     # file.close()
+#     # print('File was closed')
+#     # You can raise your own error and print a message
+#     raise KeyError("This is an error that I made up")
+#
+
+height = float(input("Height: "))
+weight = int(input("Weight: "))
+
+if height > 3:
+    raise ValueError("Human Height should not be over 3 meters.")
+
+bmi = weight / height ** 2
+print(bmi)
+
