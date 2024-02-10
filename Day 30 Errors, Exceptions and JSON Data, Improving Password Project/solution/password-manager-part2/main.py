e7120420351cdc8f38fda5f173f81372f85f3ac8
Navid Yamini <navidyamini@gmail.com>
2024-02-10 12:20:00 +0100

    Update main.py

--- Day 30 Errors, Exceptions and JSON Data, Improving Password Project/solution/password-manager-part2/main.py
+++ Day 30 Errors, Exceptions and JSON Data, Improving Password Project/solution/password-manager-part2/main.py
@@ -4,6 +4,8 @@ import random
 import json
 
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
+
+
 def generate_password():
     letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q',
                'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H',
@@ -24,7 +26,7 @@ def generate_password():
 
 
 def save():
-    website = website_entry.get()
+    website = website_entry.get().lower()
     email = email_entry.get()
     password = password_entry.get()
     new_data = {
@@ -55,7 +57,28 @@ def save():
             password_entry.delete(0, END)
 
 
+# ---------------------------- SEARCH PASSWORD ------------------------------- #
+def find_password():
+    website = website_entry.get().lower()
+    try:
+        with open("data.json", "r") as data_file:
+            # Reading data
+            data = json.load(data_file)
+    except FileNotFoundError:
+        # If the file is not found show this message box
+        messagebox.showinfo(title="ERROR", message="No data File Found!")
+    else:
+        # If the website exists in the JSON
+        if website in data:
+            email = data[website].get('email')
+            password = data[website].get('password')
+            messagebox.showinfo(title=website.upper(), message=f"Email: {email}\nPassword: {password}")
+        else:
+            messagebox.showinfo(title="Website Not Found",
+                                message=f"There is no data present in db related to {website}.")
 # ---------------------------- UI SETUP ------------------------------- #
+
+
 window = Tk()
 window.title("Password Manager")
 window.config(padx=50, pady=50)
@@ -68,25 +91,33 @@ canvas.grid(row=0, column=1)
 # labels
 website_label = Label(text="Website:")
 website_label.grid(row=1, column=0)
+
 email_label = Label(text="Email/username:")
 email_label.grid(row=2, column=0)
+
 password_label = Label(text="Password:")
 password_label.grid(row=3, column=0)
 
 # Entries
-website_entry = Entry(width=50)
-website_entry.grid(row=1, column=1, columnspan=2)
+website_entry = Entry(width=32)
+website_entry.grid(row=1, column=1)
 website_entry.focus()
+
 email_entry = Entry(width=50)
 email_entry.grid(row=2, column=1, columnspan=2)
 email_entry.insert(0, "me@email.com")
+
 password_entry = Entry(width=32)
 password_entry.grid(row=3, column=1)
 
 # Buttons
 generate_password_button = Button(text="Generate Password", width=14, command=generate_password)
 generate_password_button.grid(row=3, column=2)
+
 add_button = Button(text="Add", width=43, command=save)
 add_button.grid(row=4, column=1, columnspan=2)
+
+generate_password_button = Button(text="Search", width=14, command=find_password)
+generate_password_button.grid(row=1, column=2)
 
 window.mainloop()
