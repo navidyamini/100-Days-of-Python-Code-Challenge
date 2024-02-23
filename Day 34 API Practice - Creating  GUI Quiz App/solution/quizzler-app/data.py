0988e1dd0c2651700a3583b0fc361c015a082e47
Navid Yamini <navidyamini@gmail.com>
2024-02-23 21:34:25 +0100

    Update data.py

--- Day 34 API Practice - Creating  GUI Quiz App/solution/quizzler-app/data.py
+++ Day 34 API Practice - Creating  GUI Quiz App/solution/quizzler-app/data.py
@@ -2,7 +2,8 @@ import requests
 
 parameters = {
     "amount": 10,
-    "type": "boolean"
+    "type": "boolean",
+    "category": 18,
 }
 response = requests.get("https://opentdb.com/api.php", params=parameters)
 response.raise_for_status()
