a0c5f58d31f2874f929b3ecd103c8fefc1143e7a
Navid Yamini <navidyamini@gmail.com>
2024-02-25 09:42:55 +0100

    Update main.py

--- Day 35 Keys, Authentication and Environment Variables, Send SMS/solution/main.py
+++ Day 35 Keys, Authentication and Environment Variables, Send SMS/solution/main.py
@@ -1,7 +1,13 @@
 import requests
+from twilio.rest import Client
+import os
 
 OWM_Endpoint = "https://api.openweathermap.org/data/2.5/forecast"
-api_key = "*******************************",
+# api_key = "**********************************"
+api_key = os.environ.get("API_KEY")
+account_sid = "**********************************"
+# auth_token = "**********************************"
+auth_token = os.environ.get("AUTH_TOKEN")
 
 weather_params = {
     "appid": api_key,
@@ -20,4 +26,11 @@ for hour_data in weather_data["list"]:
     if int(condition_code) < 700:
         will_rain = True
 if will_rain:
-    print("Bring an Umbrella.")
+    client = Client(account_sid, auth_token)
+    message = client.messages \
+        .create(
+            from_='+**************',
+            body="It's going to rain today. Remember to take an umbrella ☂️ with you.",
+            to='+*****************'
+        )
+    print(message.status)
