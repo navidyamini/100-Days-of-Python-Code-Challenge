57ed5ba7db0b30365143ca95640a59c11ed25282
Navid Yamini <navidyamini@gmail.com>
2024-03-17 20:27:17 +0100

    Update main.py

--- Day 37 Habit Tracking Project API Post Request and Headers/solution/main.py
+++ Day 37 Habit Tracking Project API Post Request and Headers/solution/main.py
@@ -1,2 +1,13 @@
+import requests
 
+pixela_endpoint = "https://pixe.la/v1/users"
+
+user_params = {
+    "token": "***********************************",
+    "username": "*****************",
+    "agreeTermsOfService": "yes",
+    "notMinor": "yes",
+}
+response = requests.post(url=pixela_endpoint, json=user_params)
+print(response.text)
 
